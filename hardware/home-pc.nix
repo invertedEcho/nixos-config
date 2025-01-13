566426eaaba9e904f4ba8b3e895337bc6f034c14
Jakob Stechow <jakob.stechow@pm.me>
2025-01-13 21:42:06 +0100

    fix: disable sleep/suspend/hibernation on home-pc bcs nvidia graphical corruption issues after resume

--- hardware/home-pc.nix
+++ hardware/home-pc.nix
@@ -10,6 +10,12 @@
     ../modules/nvidia.nix
   ];
 
+  # graphical corruption bcs nvidia after resume from suspend
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   boot = {
     kernelModules = ["kvm-amd"];
     initrd = {
