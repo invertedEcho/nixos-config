584dc67002ac94d2467817884e728bbaf31c1d60
Jakob Stechow <jakob.stechow@pm.me>
2025-02-22 23:36:25 +0100

    feat!: remove gnome

--- modules/services.nix
+++ modules/services.nix
@@ -7,13 +7,6 @@
     gnome.gnome-keyring.enable = true;
     xserver = {
       enable = true;
-      displayManager.gdm = {
-        enable = true;
-        wayland = true;
-      };
-      desktopManager = {
-        gnome.enable = true;
-      };
     };
     pipewire = {
       enable = true;
