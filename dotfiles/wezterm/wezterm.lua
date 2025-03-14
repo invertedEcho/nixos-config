a87e9decf9991fc35335d74ed63e33dc9e21b35d
Jakob Stechow <jakob.stechow@pm.me>
2025-03-14 11:15:15 +0100

    fix: terminal on linux

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
 	color_scheme = "Gruvbox Dark (Gogh)",
 	font = wezterm.font("Terminess Nerd Font Mono"),
 	font_size = utils.is_linux() and 12 or 18,
-	-- front_end = "WebGpu",
+	front_end = "WebGpu",
 	hide_tab_bar_if_only_one_tab = true,
-	enable_wayland = true,
+	enable_wayland = false,
 }
