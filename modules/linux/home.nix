572dccc76a559e983bfc0149673e55c747e4d8a5
Jakob Stechow <jakob.stechow@pm.me>
2025-05-06 15:10:08 +0200

    chore(linux): update dotfiles and add wallpapers

--- modules/linux/home.nix
+++ modules/linux/home.nix
@@ -26,18 +26,21 @@
         source = pkgs.fetchFromGitHub {
           owner = "invertedEcho";
           repo = "dotfiles";
-          rev = "633a85c08433";
-          hash = "sha256-qWcudXIxvq7iQkF9TU0y7k+U9D6pia3S0tE9D8KZPWY=";
+          rev = "9932f9af3015";
+          hash = "sha256-t2oVIlvfRBOMCkwlMxaF6ep0rYvLVwvw08vTFDnCnCI=";
         };
         recursive = true;
+      };
+      "Pictures/Wallpapers" = {
+        source = ../../media/wallpapers;
       };
       "${config.xdg.configHome}/nvim" = {
         source = pkgs.runCommand "nvim-config" {} ''
           src=${pkgs.fetchFromGitHub {
             owner = "invertedEcho";
             repo = "nvim-config";
-            rev = "3b5c72be137dcd867ec48f8dd4c59a5723e2631a";
-            hash = "sha256-8l+IP46MLMq2MsiHx4T7niTwfMdz+YQD0645/fmaB8M=";
+            rev = "88f622e70bd0d3cb110cac5996db893bb7214061";
+            hash = "sha256-hpJ+BuWUzxcjyRAHJA8vIDEbeSQKHY+lwWa+qckztf0=";
           }}
 
           mkdir -p $out
