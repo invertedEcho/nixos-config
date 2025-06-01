333c364820401b44428b7a631dfa091453f1a7b2
Jakob Stechow <jakob.stechow@pm.me>
2025-06-01 14:35:40 +0200

    feat: sddm sugar-dark theme

--- modules/linux/desktop-environments/plasma.nix
+++ modules/linux/desktop-environments/plasma.nix
@@ -1,9 +1,18 @@
 {pkgs, ...}: {
   services = {
     xserver.enable = true;
-    displayManager.sddm.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      theme = "sugar-dark";
+      package = pkgs.lib.mkForce pkgs.libsForQt5.sddm;
+      extraPackages = pkgs.lib.mkForce [pkgs.libsForQt5.qt5.qtgraphicaleffects];
+    };
     desktopManager.plasma6.enable = true;
   };
+
+  environment.defaultPackages = with pkgs; [
+    sddm-sugar-dark
+  ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     dolphin
