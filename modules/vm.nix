47440cd1a9c9ba454bca793197faa0df1b588383
Jakob Stechow <jakob.stechow@pm.me>
2025-06-11 20:47:29 +0200

    fix: incorrect attribute set for vm module

--- modules/vm.nix
+++ modules/vm.nix
@@ -2,7 +2,7 @@
   programs.virt-manager.enable = true;
   # TODO: Username should come from variable set somewhere
   users.groups.libvirt.members = ["echo"];
-  services = {
+  virtualisation = {
     libvirtd.enable = true;
     spiceUSBRedirection.enable = true;
   };
