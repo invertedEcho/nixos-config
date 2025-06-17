f8f4ac845b149a3a64a1cfa5eabc74b19219e152
Jakob Stechow <jakob.stechow@pm.me>
2025-06-17 16:48:14 +0200

    fix: remove iwd as gnome already provides networkmanager

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -34,8 +34,6 @@
     nerd-fonts.jetbrains-mono
   ];
 
-  networking.wireless.iwd.enable = true;
-
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
   security.polkit.enable = true;
