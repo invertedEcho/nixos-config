eb43169039c01753c098db29ec5376859e75d561
Jakob Stechow <jakob.stechow@pm.me>
2025-06-29 22:51:03 +0200

    fix(tmux): relevant options for nvim in tmux

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -4,6 +4,8 @@
       enable = true;
       extraConfig = ''
         set -as terminal-features ",xterm-256color:RGB"
+        set-option -sg escape-time 10
+        set-option -g default-terminal "screen-256color"
       '';
       # Sorry father for what i have sinned
       mouse = true;
