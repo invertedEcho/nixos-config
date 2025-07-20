970b4afd66d487f3056c691f270699e240e84527
Jakob Stechow <jakob.stechow@pm.me>
2025-07-20 16:29:47 +0200

    feat(tmuxinator): add platformer-bevy-ldkt config

--- modules/home-manager/tmuxinator.nix
+++ modules/home-manager/tmuxinator.nix
@@ -1,16 +1,32 @@
 {
-  home.file.".tmuxinator/endless-scroller-bevy.yml" = {
-    text = ''
-      name: endless-scroller-bevy
-      root: ~/dev/endless-scroller-bevy
+  home.file = {
+    ".tmuxinator/endless-scroller-bevy.yml" = {
+      text = ''
+        name: endless-scroller-bevy
+        root: ~/dev/endless-scroller-bevy
 
-      windows:
-       - editor:
-         - nvim
-       - git:
-         - lazygit
-       - cargo:
-         - cargo run
-    '';
+        windows:
+         - editor:
+           - nvim
+         - git:
+           - lazygit
+         - cargo:
+           - cargo run
+      '';
+    };
+    ".tmuxinator/platformer-bevy-ldtk.yml" = {
+      text = ''
+        name: platformer-bevy-ldtk
+        root: "~/dev/platformer-bevy-ldtk"
+
+        windows:
+          - editor:
+            - nvim
+          - git:
+            - lazygit
+          - cargo:
+            - cargo run
+      '';
+    };
   };
 }
