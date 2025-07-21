a2d181c5b52e36d0c547068c8afc08c786c5c2b8
Jakob Stechow <jakob.stechow@pm.me>
2025-07-21 16:23:28 +0200

    feat(dconf): color scheme prefer dark

--- modules/home-manager/dconf.nix
+++ modules/home-manager/dconf.nix
@@ -1,5 +1,8 @@
 {
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
     "org/virt-manager/virt-manager/connections" = {
       autoconnect = ["qemu:///system"];
       uris = ["qemu:///system"];
