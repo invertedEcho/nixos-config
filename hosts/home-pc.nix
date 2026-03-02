d7554280658cfaa21947b7e323edbd01fcba7735
Jakob Stechow <jakob.stechow@pm.me>
2026-03-02 10:29:54 +0100

    fix: disable sleep/suspend/hibernate for home-pc as wake-up is broken

--- hosts/home-pc.nix
+++ hosts/home-pc.nix
@@ -11,6 +11,12 @@
     ../modules/bluetooth.nix
   ];
 
+  # wake up doesnt work with nvidia
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
