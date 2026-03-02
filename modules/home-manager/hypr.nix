746ea56e7737ac67c63877bdf3eb37728f3f0738
Jakob Stechow <jakob.stechow@pm.me>
2026-03-02 10:31:57 +0100

    feat: merge hypr module and enable hyprpolkitagent

---
+++ modules/home-manager/hypr.nix
@@ -0,0 +1,22 @@
+{
+  services = {
+    hyprpolkitagent.enable = true;
+    hypridle = {
+      enable = true;
+      settings = {
+        listener = [
+          {
+            # 10 min
+            timeout = 600;
+            on-timeout = "hyprlock";
+          }
+          {
+            # 15 min
+            timeout = 900;
+            on-timeout = "systemctl suspend-then-hibernate";
+          }
+        ];
+      };
+    };
+  };
+}
