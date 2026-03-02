383d8a98e4ab05b370fbd0945b83bc9b6c56ed56
Jakob Stechow <jakob.stechow@pm.me>
2026-03-02 10:29:18 +0100

    feat: set LC_TIME (helps waybar calendar module among other things)

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -3,7 +3,12 @@
 
   time.timeZone = "Europe/Berlin";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
 
   users.users.echo = {
     isNormalUser = true;
