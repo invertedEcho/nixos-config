c21ae52d9e41e3004ae1d0964e6765deb70eb6d3
Jakob Stechow <jakob.stechow@pm.me>
2026-03-10 17:04:55 +0100

    feat: specify default application for mime types

--- modules/home-manager/xdg.nix
+++ modules/home-manager/xdg.nix
@@ -1,9 +1,19 @@
 {pkgs, ...}: {
-  xdg.autostart = {
-    enable = true;
-    entries = [
-      # "${pkgs.spotify}/share/applications/spotify.desktop"
-      # "${pkgs._1password-gui}/share/applications/1password.desktop"
-    ];
+  xdg = {
+    autostart = {
+      enable = true;
+      entries = [
+        # "${pkgs.spotify}/share/applications/spotify.desktop"
+        # "${pkgs._1password-gui}/share/applications/1password.desktop"
+      ];
+    };
+    # https://wiki.nixos.org/wiki/Default_applications
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/png" = "org.gnome.Loupe.desktop";
+        "image/jpeg" = "org.gnome.Loupe.desktop";
+      };
+    };
   };
 }
